4c29c54ab07660459c7e5542f429f00052cfe619
Joey Beninghove <me@joey.io>
2016-11-24 22:34:17 -0500

    Add ability to create invoice and redirect
    This also includes the code to show the payment option on the checkout
    page.
    
    Closes #2
    Closes #3

--- cloudswipe.php
+++ cloudswipe.php
@@ -2,35 +2,37 @@
 
 use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
 
-if (!defined('_PS_VERSION_')) {
+if (!defined("_PS_VERSION_")) {
     exit;
 }
 
 class CloudSwipe extends PaymentModule
 {
+    public $secretKey = "sk_store_68b8bfedbea80675fcf1374e";
+
     public function __construct()
     {
-        $this->name = 'cloudswipe';
-        $this->tab = 'payments_gateways';
-        $this->version = '1.0.0';
+        $this->name = "cloudswipe";
+        $this->tab = "payments_gateways";
+        $this->version = "1.0.0";
         $this->ps_versions_compliancy = [
-            'min' => '1.7',
-            'max' => _PS_VERSION_
+            "min" => "1.7",
+            "max" => _PS_VERSION_
         ];
-        $this->author = 'Joey Beninghove';
-        $this->controllers = ['validation'];
+        $this->author = "Joey Beninghove";
+        $this->controllers = ["createinvoice"];
         $this->bootstrap = true;
 
         parent::__construct();
 
-        $this->displayName = $this->l('CloudSwipe');
-        $this->description = $this->l('Accepts payments through CloudSwipe');
+        $this->displayName = $this->l("CloudSwipe");
+        $this->description = $this->l("Accepts payments through CloudSwipe");
     }
 
     public function install()
     {
         if (!parent::install() ||
-            !$this->registerHook('paymentOptions')) {
+            !$this->registerHook("paymentOptions")) {
             return false;
         }
 
@@ -44,11 +46,11 @@ class CloudSwipe extends PaymentModule
         }
 
         $link = $this->context->link->getModuleLink(
-            $this->name, 'validation', [], true
+            $this->name, "createinvoice", [], true
         );
 
         $option = new PaymentOption();
-        $option->setCallToActionText($this->l('Pay by Credit Card'))
+        $option->setCallToActionText($this->l("Pay by Credit Card"))
                ->setAction($link);
 
         return [$option];
---
+++ controllers/front/createinvoice.php
@@ -0,0 +1,104 @@
+<?php
+
+require __DIR__."/../../vendor/autoload.php";
+
+class CloudSwipeCreateInvoiceModuleFrontController extends ModuleFrontController
+{
+    public function postProcess()
+    {
+        \CloudSwipe\CloudSwipe::setSecretKey("sk_store_68b8bfedbea80675fcf1374e");
+
+        $cart = $this->context->cart;
+        $currency = Currency::getCurrencyInstance((int)$cart->id_currency);
+
+        $invoice = \CloudSwipe\Invoice::create([
+            "total" => $cart->getOrderTotal() * 100,
+            "currency" => $currency->iso_code,
+            "customer" => $this->customerJSON($cart),
+            "line_items" => $this->lineItemsJSON($cart, $currency),
+            "line_totals" => $this->lineTotalsJSON($cart, $currency)
+        ]);
+
+        Tools::redirect("http://joey.cloudswipe.dev/pay/" . $invoice->id);
+    }
+
+    private function fullName($object)
+    {
+        return implode(" ", [$object->firstname, $object->lastname]);
+    }
+
+    private function lineTotalsJSON($cart, $currency)
+    {
+        $summary = $cart->getSummaryDetails();
+
+        return [
+            "rows" => [
+                "Discount" => Tools::displayPrice(
+                    $summary["total_discounts"], $currency),
+                "Shipping" => Tools::displayPrice(
+                    $summary["total_shipping"], $currency),
+                "Tax" => Tools::displayPrice(
+                    $summary["total_tax"], $currency)
+            ]
+        ];
+    }
+
+    private function lineItemsJSON($cart, $currency)
+    {
+        $products = $cart->getProducts();
+
+        $rows = [];
+        foreach ($products as $product) {
+            $rows[] = $this->lineItemRowJSON($product, $currency);
+        }
+
+        return [
+            "header" => ["Item", "Description", "Unit Price",
+                         "Quantity", "Total"],
+            "rows" => $rows
+        ];
+    }
+
+    private function lineItemRowJSON($product, $currency)
+    {
+        return [
+            $product["name"],
+            $product["attributes_small"],
+            Tools::displayPrice($product["price"], $currency),
+            $product["cart_quantity"],
+            Tools::displayPrice($product["total"], $currency)
+        ];
+    }
+
+    private function customerJSON($cart)
+    {
+        $customer = new Customer($cart->id_customer);
+        $billingAddress = new Address((int)$cart->id_address_invoice);
+        $shippingAddress = new Address((int)$cart->id_address_delivery);
+
+        return [
+            "name" => $this->fullName($customer),
+            "email" => $customer->email,
+            "billing_address" => $this->addressJSON($billingAddress),
+            "shipping_address" => $this->addressJSON($shippingAddress)
+        ];
+    }
+
+    private function addressJSON($address)
+    {
+        $state = new State((int)$address->id_state);
+        $country = new Country((int)$address->id_country);
+
+        return [
+            "name" => $this->fullName($address),
+            "company" => $address->company,
+            "line1" => $address->address1,
+            "line2" => $address->address2,
+            "city" => $address->city,
+            "state" => $state->iso_code,
+            "country" => $country->iso_code,
+            "zip" => $address->postcode,
+            "phone" => $address->phone
+        ];
+    }
+}

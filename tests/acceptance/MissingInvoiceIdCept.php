7fe8c343b19499cd0257215bb69b0139aab02530
Joey Beninghove <me@joey.io>
2017-02-16 12:09:33 -0500

    Removing non-production code from repo
    This is necessary to make the PrestaShop validator tool happy.

--- tests/acceptance/MissingInvoiceIdCept.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$I = new AcceptanceTester($scenario);
-$I->wantTo("verify receipt page failure with missing invoice id");
-
-$I->amOnPage("/module/cloudswipe/receipt");
-$I->see("Missing invoice id");
--- tests/acceptance/UnknownInvoiceCept.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
-$I = new AcceptanceTester($scenario);
-$I->wantTo("verify receipt failure for unknown invoice id");
-
-$I->amOnPage("/module/cloudswipe/receipt?invoice_id=foobar");
-$I->see("invoice foobar not found");

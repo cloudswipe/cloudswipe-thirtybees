b0e307d2594c2853b14b34fd77b692c16f786bd0
Joey Beninghove <me@joey.io>
2017-02-28 12:27:11 -0500

    Few more fixes for the PrestaShop validator

--- cloudswipepayments.php
+++ cloudswipepayments.php
@@ -1,8 +1,30 @@
 <?php
 
-require_once(dirname(__FILE__)."/lib/CloudSwipe.php");
+/**
+* The MIT License (MIT)
+* 
+* Copyright (c) 2017 CloudSwipe
+* 
+* Permission is hereby granted, free of charge, to any person obtaining a copy
+* of this software and associated documentation files (the "Software"), to deal
+* in the Software without restriction, including without limitation the rights
+* to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+* copies of the Software, and to permit persons to whom the Software is
+* furnished to do so, subject to the following conditions:
+* 
+* The above copyright notice and this permission notice shall be included in all
+* copies or substantial portions of the Software.
+* 
+* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+* IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+* FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+* AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+* LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+* OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+* SOFTWARE.
+*/
 
-use PrestaShop\PrestaShop\Core\Payment\PaymentOption;
+require_once(dirname(__FILE__)."/lib/CloudSwipe.php");
 
 if (!defined("_PS_VERSION_")) {
     exit;
@@ -52,7 +74,7 @@ class CloudSwipePayments extends PaymentModule
             $this->name, "invoice", [], true
         );
 
-        $option = new PaymentOption();
+        $option = new PrestaShop\PrestaShop\Core\Payment\PaymentOption();
         $option->setCallToActionText($this->l("Pay by Credit Card"))
                ->setAction($link);
 
@@ -65,7 +87,7 @@ class CloudSwipePayments extends PaymentModule
 
         if (Tools::isSubmit("submit".$this->name))
         {
-            $secretKey = strval(Tools::getValue("CLOUDSWIPE_SECRET_KEY"));
+            $secretKey = (string) Tools::getValue("CLOUDSWIPE_SECRET_KEY");
             if (!$secretKey
                 || empty($secretKey)
                 || !Validate::isGenericName($secretKey))
@@ -83,9 +105,10 @@ class CloudSwipePayments extends PaymentModule
     public function displayForm()
     {
         // Get default language
-        $default_lang = (int)Configuration::get("PS_LANG_DEFAULT");
+        $default_lang = (int) Configuration::get("PS_LANG_DEFAULT");
 
         // Init Fields form array
+        $fields_form = [];
         $fields_form[0]["form"] = array(
             "legend" => array(
                 "title" => $this->l("Settings"),
